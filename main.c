14658fd5daad193664d00520c1ad6996042b16cf
ifizmat <ifizmat@gmail.com>
2021-11-22 13:16:56 +0400

    jija

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{int yearbirds;
+int currentyear;
+int age;
+yearbirds=1943;
+currentyear=2010;
+age=currentyear-yearbirds;
+printf("age of student: %d years old.\n",age);
+return 0;
+}
+
